e5c2c2279bb6525232d5bcfa054164ce4bcd1d0a
swasthikkj <99305988+swasthikkj@users.noreply.github.com>
2022-08-06 18:05:17 +0530

    Added Use Cases

---
+++ helloweb/src/main/java/com/bl/helloweb/controller/HelloRestController.java
@@ -0,0 +1,44 @@
+package com.bl.helloweb.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bl.helloweb.model.User;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+	
+    @RequestMapping(value = {"", "/", "/home"})
+    @GetMapping("/getdata")
+    public String sayHello() {
+        return "Hello from bridgelabz";
+    }
+    
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name){
+        return "Hello" + name + "!";
+    }
+    
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name){
+        return "Hello" + name + "!";
+    }
+    
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user){
+        return "Hello" + user.getFirstName() + " " + user.getLastName() + "!";
+    }
+    
+    @PutMapping("/put/{firstName}")
+    public String sayHello(@PathVariable String firstName, @RequestParam(value = "lastName") String lastName){
+        return "Hello" + firstName + " " + lastName + "!";
+    }
+}
